160f688e4ca061c81b3a8e03b62d5666dc47d72a
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2022-09-21 02:41:03 +0530

    Changed a lot

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import Link from "next/link";
+import Layout from "../components/layout";
 import { getSortedPostsData } from "../lib/posts";
+import Date from "../components/date";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -13,49 +15,26 @@ export async function getStaticProps() {
 
 export default function Home({ allPostsData }) {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-0 px-2">
+    <Layout>
       <Head>
-        <title>Create Next App</title>
+        <title>Snippets</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <h1 className="text-3xl font-bold underline">Snippets</h1>
-
-      <main className="flex flex-col items-center justify-center flex-grow px-0 py-20">
-        <h1 className="title">
-          Read <Link href="/posts/first-post">this page!</Link>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-        <section>
-          <h2>Blog</h2>
-          <ul>
-            {allPostsData.map(({ id, date, title }) => (
-              <li key={id}>
-                <Link href={`/posts/${id}`}>
-                  <a>{title}</a>
-                </Link>
-                <br />
-                <p>
-                  <Date dateString={date} />
-                </p>
-              </li>
-            ))}
-          </ul>
-        </section>
-      </main>
-
-      <footer className="border-solid border-t flex items-center justify-center h-24 w-full">
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
-    </div>
+      <section>This is section</section>
+      <h1>Snippets</h1>
+      <ul>
+        {allPostsData.map(({ id, date, title }) => (
+          <li key={id}>
+            {" "}
+            <Date dateString={date} />
+            <Link href={`/posts/${id}`}>
+              <a>
+                <h2>{title}</h2>
+              </a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </Layout>
   );
 }
---
+++ posts/hello-world.md
@@ -0,0 +1,12 @@
+---
+title: "Hello World"
+date: "2020-03-01"
+---
+
+# Hello World
+
+## Description
+
+### Wow this is pretty simple
+
+Hello World is the best way to start a web application without having to worry about the configuration settings that are required for the application to render. Here are some examples of how to configure the application without having to worry about the configuration settings that are required for the application to render: 1 - render the application without having to worry about the configuration settings that are required for the application to Rendering
