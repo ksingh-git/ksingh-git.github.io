b1a8f6fd859cac73c8794e0308756f85d449ad8c
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2022-10-10 22:29:50 +0530

    Dark Mode

--- components/layout.js
+++ components/layout.js
@@ -1,3 +1,37 @@
+import { useEffect, useState } from "react";
+
 export default function Layout({ children }) {
-  return <div>{children}</div>;
+  const [theme, setTheme] = useState("light");
+
+  const darkMode = () => {
+    let element = document.getElementById("main-container");
+    element.classList.toggle("dark");
+    if (localStorage.getItem("theme") === "dark") {
+      setTheme("light");
+      localStorage.setItem("theme", "light");
+    } else {
+      setTheme("dark");
+      localStorage.setItem("theme", "dark");
+    }
+  };
+
+  useEffect(() => {
+    setTheme(localStorage.getItem("theme"));
+    const element = document.getElementById("main-container");
+    element.classList.add(theme);
+  });
+
+  return (
+    <>
+      <>{theme}</>
+      <button
+        onClick={() => {
+          darkMode();
+        }}
+      >
+        Dark Mode
+      </button>
+      <div id="main-container">{children}</div>
+    </>
+  );
 }
