b1ff2906efe7215e69c0a9c3b3e99be6f2aa8e0d
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2022-10-19 09:23:18 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -40,7 +40,6 @@ export default function Home({ allPostsData, allCategories }) {
           <h1 className="mx-auto max-w-screen-sm text-center mb-4 text-7xl underline tracking-tight font-extrabold text-gray-900 dark:text-white">
             Snippets
           </h1>
-          <h1>{selectedCategory}</h1>
           <div className=" p-8 cursor-pointer flex absolute top-0 right-0">
             <ThemeModeSvgComponent />
           </div>
@@ -54,7 +53,7 @@ export default function Home({ allPostsData, allCategories }) {
                 type="button"
                 className="py-2.5 px-5 mr-2 mb-2 text-lg font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-400 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
               >
-                All
+                ALL
               </button>
             </li>
             {allCategories.map((category) => (
@@ -73,17 +72,20 @@ export default function Home({ allPostsData, allCategories }) {
           </ul>
           <div className="grid gap-8 lg:grid-cols-2">
             {allPostsData.map(
-              ({ id, date, title, description, language, categories }) => (
-                <ItemPost
-                  key={id}
-                  id={id}
-                  date={date}
-                  title={title}
-                  description={description}
-                  language={language}
-                  categories={categories}
-                />
-              )
+              ({ id, date, title, description, language, categories }) => {
+                return selectedCategory === "" ||
+                  categories.includes(selectedCategory) ? (
+                  <ItemPost
+                    key={id}
+                    id={id}
+                    date={date}
+                    title={title}
+                    description={description}
+                    language={language}
+                    categories={categories}
+                  />
+                ) : null;
+              }
             )}
           </div>
         </div>
