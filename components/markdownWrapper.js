2e5342ab97449acbded236a75ad8bfc64b124f22
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2022-12-12 23:29:56 +0530

    Update markdownWrapper.js

--- components/markdownWrapper.js
+++ components/markdownWrapper.js
@@ -1,6 +1,10 @@
 export default function MarkDownWrapper({ children }) {
   return (
-    <section className="prose prose-lg prose-slate prose-h2:underline prose-h3:underline prose-pre:overflow-hidden dark:prose-invert md:prose-2xl">
+    <section
+      className="prose prose-2xl prose-slate 
+    prose-h2:underline prose-h3:underline prose-pre:overflow-hidden 
+    dark:prose-invert md:prose-2xl"
+    >
       {children}
     </section>
   );
