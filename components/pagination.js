af2cd9c664e81a33b1ceca89f1948193322501d0
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2023-08-28 04:15:08 +0530

    Update pagination.js

--- components/pagination.js
+++ components/pagination.js
@@ -9,10 +9,12 @@ const Pagination = ({ currentPage, totalPages }) => {
       <nav aria-label="Page navigation example">
         <ul class="inline-flex h-10 -space-x-px text-base">
           {pages.map((page) => (
-            <Link href={`/page/${page}`} key={page}>
+            <Link href={`/content/${page}`} key={page}>
               <a
                 className={`pagination-link ${
-                  page === currentPage ? "active" : ""
+                  page === currentPage
+                    ? "flex h-10 items-center justify-center border border-gray-300 bg-blue-50 px-4 text-blue-600 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
+                    : "flex h-10 items-center justify-center border border-gray-300 bg-white px-4 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
                 }`}
               >
                 {page}
