1bd8db41f2d78857be1aee244744cf311afe9260
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2023-08-27 18:04:24 +0530

    Update copyFunctions.js

--- components/copyFunctions.js
+++ components/copyFunctions.js
@@ -1,34 +1,29 @@
 export const copyToClipboard = (str) => {
-  const el = document.createElement("textarea");
-  el.value = str;
-  el.setAttribute("readonly", "");
-  el.style.position = "absolute";
-  el.style.left = "-9999px";
-  document.body.appendChild(el);
-  const selected =
-    document.getSelection().rangeCount > 0
-      ? document.getSelection().getRangeAt(0)
-      : false;
-  el.select();
-  document.execCommand("copy");
-  document.body.removeChild(el);
-  if (selected) {
-    document.getSelection().removeAllRanges();
-    document.getSelection().addRange(selected);
-  }
+  navigator.clipboard
+    .writeText(str)
+    .then(() => {
+      console.log("Text copied to clipboard:", str);
+    })
+    .catch((error) => {
+      console.error("Unable to copy text:", error);
+    });
 };
 
 export function handleCopyClick(evt) {
   const { children } = evt.target.parentElement;
   const { innerText } = Array.from(children)[0];
   copyToClipboard(innerText);
-  alert("copied : " + innerText);
+  this.innerHTML = "Copied!";
+  // After 2 seconds, change the innerHTML back to "Copy"
+  setTimeout(() => {
+    this.innerHTML = "Copy";
+  }, 2000);
 }
 
 export function addButtonToPre() {
   const copy = document.createElement("button");
   copy.innerHTML = "Copy";
-  copy.classList.add();
+  copy.classList.add("copy-button");
   copy.addEventListener("click", handleCopyClick);
   return copy;
 }
