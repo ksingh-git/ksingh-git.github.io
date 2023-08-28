1153333d6cfd0ba66277fd9f120b29f621da4871
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2023-08-28 18:05:56 +0530

    Update [page].js

--- pages/content/[page].js
+++ pages/content/[page].js
@@ -1,27 +1,72 @@
 import { useRouter } from "next/router";
 import Pagination from "../../components/pagination";
 import { getPaginatedPosts, getTotalPosts } from "../../lib/posts";
-
+import ItemPost from "../../components/itemPost";
+import Head from "next/head";
+import Layout from "../../components/layout";
+import ThemeModeSvgComponent from "../../components/theme_mode";
 const PostsPage = ({ posts, totalPages }) => {
   const router = useRouter();
   const currentPage = Number(router.query.page) || 1;
 
   return (
-    <div>
-      {/* Render your list of posts */}
-      {posts.map((post) => (
-        // Render post content
-        <div>Mo</div>
-      ))}
+    <Layout>
+      <Head>
+        <title>Snippets</title>
+        <link
+          rel="icon"
+          href="https://img.icons8.com/emoji/256/four-leaf-clover.png"
+        />
+      </Head>
+      <div className="flex min-w-min cursor-default content-center justify-center align-middle">
+        <section className="relative bg-white dark:bg-gray-900 md:m-8 md:w-full">
+          <div className="relative mx-auto my-8 max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16">
+            <div className="absolute right-2 top-0 md:hidden">
+              <ThemeModeSvgComponent className="cursor-pointer" />
+            </div>
+            <h1
+              className="mx-auto mb-4 max-w-screen-sm select-none text-center text-7xl font-extrabold tracking-tight
+           text-gray-900 underline dark:text-white"
+            >
+              Snippets
+            </h1>
 
-      {/* Render the Pagination component */}
-      <Pagination currentPage={currentPage} totalPages={totalPages} />
-    </div>
+            {/* Posts */}
+            <div className="min-w-200 grid gap-8 lg:grid-cols-2">
+              {posts.map(
+                ({ id, date, title, description, language, categories }) => {
+                  return (
+                    <ItemPost
+                      key={id}
+                      id={id}
+                      date={date}
+                      title={title}
+                      description={description}
+                      language={language}
+                      categories={categories}
+                    />
+                  );
+                }
+              )}
+            </div>
+            {/* Render the Pagination component */}
+            <div
+              className="mx-auto mb-4 max-w-screen-sm select-none text-center text-7xl font-extrabold tracking-tight
+           text-gray-900 underline dark:text-white"
+            >
+              {totalPages > 1 ? (
+                <Pagination currentPage={currentPage} totalPages={totalPages} />
+              ) : null}
+            </div>
+          </div>
+        </section>
+      </div>
+    </Layout>
   );
 };
 
 export async function getServerSideProps(context) {
-  const postsPerPage = 4; // Number of posts to display per page
+  const postsPerPage = 8; // Number of posts to display per page
   const page = context.query.page || 1;
   const posts = await getPaginatedPosts(page, postsPerPage);
   const totalPosts = await getTotalPosts(); // Implement this function to get the total number of posts
