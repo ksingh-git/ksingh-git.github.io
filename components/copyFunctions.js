eab5e3d33bc490de8a36891bb0c5007c6712cfd5
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2023-11-24 16:49:11 +0530

    Update copyFunctions.js

--- components/copyFunctions.js
+++ components/copyFunctions.js
@@ -1,3 +1,9 @@
+/**
+ * Copies the given string to the clipboard.
+ *
+ * @param {string} str - The string to be copied to the clipboard.
+ * @return {Promise<void>} A promise that resolves when the text has been successfully copied to the clipboard.
+ */
 export const copyToClipboard = (str) => {
   navigator.clipboard
     .writeText(str)
@@ -9,6 +15,12 @@ export const copyToClipboard = (str) => {
     });
 };
 
+/**
+ * Handles the click event for the copy button.
+ *
+ * @param {Event} evt - The click event object.
+ * @return {undefined} This function does not return any value.
+ */
 export function handleCopyClick(evt) {
   const { children } = evt.target.parentElement;
   const { innerText } = Array.from(children)[0];
@@ -20,6 +32,11 @@ export function handleCopyClick(evt) {
   }, 2000);
 }
 
+/**
+ * Creates a new button element and adds it to the pre element.
+ *
+ * @return {HTMLButtonElement} The newly created button element.
+ */
 export function addButtonToPre() {
   const copy = document.createElement("button");
   copy.innerHTML = "Copy";
