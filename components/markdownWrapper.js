a5c9e3a20062c51fcd4e23043c48c9b8cf4b3f1a
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2024-10-31 16:23:45 +0530

    Update markdownWrapper.js

--- components/markdownWrapper.js
+++ components/markdownWrapper.js
@@ -1,10 +1,21 @@
+import PropTypes from "prop-types";
+/**
+ * A wrapper component for rendering Markdown content using Tailwind's prose class.
+ *
+ * This component is intended to be used as a direct child of a page component,
+ * and should not be used as a direct child of a route component.
+ *
+ * @param {React.ReactNode} children The Markdown content to be rendered.
+ * @returns {JSX.Element} The rendered Markdown content.
+ */
 export default function MarkDownWrapper({ children }) {
   return (
-    <section
-      className="prose prose-2xl prose-slate max-w-none 
-    prose-h2:underline prose-h3:underline prose-pre:overflow-hidden dark:prose-invert "
-    >
+    <section className="prose prose-2xl prose-slate max-w-none dark:prose-invert prose-h2:underline prose-h3:underline prose-pre:overflow-hidden ">
       {children}
     </section>
   );
 }
+
+MarkDownWrapper.propTypes = {
+  children: PropTypes.node.isRequired,
+};
