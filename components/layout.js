f3a6ec93e7c0b33e414f19d6b6bbb300cfa7029e
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2024-10-31 16:24:53 +0530

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,16 @@
 import Head from "next/head";
+import PropTypes from "prop-types";
 import { useEffect, useState } from "react";
 
+/**
+ * The main layout component for the site, setting the page title and theme,
+ * and wrapping the children with a container element that is styled with
+ * the current theme.
+ *
+ * @param {Object} props - The properties passed to the component.
+ * @param {React.ReactNode} props.children - The children to be rendered.
+ * @returns {ReactElement} - The rendered component.
+ */
 export default function Layout({ children }) {
   const [theme, setTheme] = useState("light");
   useEffect(() => {
@@ -14,7 +24,7 @@ export default function Layout({ children }) {
           href="https://img.icons8.com/emoji/256/four-leaf-clover.png"
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Indie+Flower&family=Whisper&display=swap"
           rel="stylesheet"
@@ -26,3 +36,7 @@ export default function Layout({ children }) {
     </>
   );
 }
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
