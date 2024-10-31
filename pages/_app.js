331ba6991b8e0dd54af367d8bd89b39f2a5644bb
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2024-10-31 16:11:51 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
 import "../styles/globals.css";
+import propTypes from "prop-types";
 
+/**
+ * The main entry point for the Next.js application.
+ *
+ * @param {Object} props - The properties passed to the component.
+ * @param {React.ComponentType} props.Component - The active page component to render.
+ * @param {Object} props.pageProps - The initial props that were preloaded for the page.
+ * @returns {ReactElement} The rendered component for the active page.
+ */
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+
+App.propTypes = {
+  Component: propTypes.elementType.isRequired,
+  pageProps: propTypes.object.isRequired,
+};
