7eaa7acf0e39e0e97dcd3a1a10128037f3b10011
Kamal Singh <37046527+ksingh-git@users.noreply.github.com>
2025-04-13 16:44:28 +0530

    Minor Changes

--- pages/404.js
+++ pages/404.js
@@ -21,7 +21,13 @@ export default function Custom404() {
             <Link href={"/"}>
               <button
                 type="button"
-                class="mb-2 mr-2 rounded-full border border-gray-300 bg-white px-5 py-2.5 text-base font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700 md:text-lg"
+                className="
+                  mb-2 mr-2 rounded-full border border-gray-300 bg-white px-5 py-2.5 
+                  text-base font-medium text-gray-900 hover:bg-gray-100 focus:outline-none 
+                  focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 
+                  dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 
+                  dark:focus:ring-gray-700 md:text-lg
+                "
               >
                 Back to Homepage
               </button>
