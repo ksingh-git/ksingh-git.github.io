69647d0a21f6b3092bb92677a35c8f30440c0d98
Kamal Singh <37046527+ksingh-git@users.noreply.github.com>
2025-08-27 19:15:32 +0530

    Changes

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Layout from "../components/layout";
 import Hero from "../ui/hero";
+
 export default function Home() {
-  return (
-    <Layout>
-      <Hero />
-    </Layout>
-  );
+    return (
+        <Layout>
+            <Hero/>
+        </Layout>
+    );
 }
--- ui/hero.js
+++ ui/hero.js
@@ -7,23 +7,23 @@ const Hero = () => {
         <div className="absolute right-2 top-0">
           <ThemeModeSvgComponent className="cursor-pointer" />
         </div>
-        <section class="bg-white dark:bg-gray-900">
-          <div class="mx-auto grid max-w-screen-xl px-4 py-4 lg:grid-cols-12 lg:gap-8 lg:py-8 xl:gap-0">
-            <div class="mr-auto place-self-center lg:col-span-7">
-              <h1 class="mb-4 max-w-2xl text-5xl font-extrabold leading-none tracking-tight dark:text-white md:text-6xl xl:text-8xl">
+        <section className="bg-white dark:bg-gray-900">
+          <div className="mx-auto grid max-w-screen-xl px-4 py-4 lg:grid-cols-12 lg:gap-8 lg:py-8 xl:gap-0">
+            <div className="mr-auto place-self-center lg:col-span-7">
+              <h1 className="mb-4 max-w-2xl text-5xl font-extrabold leading-none tracking-tight dark:text-white md:text-6xl xl:text-8xl">
                 Kamal Singh
               </h1>
-              <p class="mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl">
+              <p className="mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl">
                 Software Engineer with a passion for building scalable web
                 applications and working across the full stack.
               </p>
               <a
                 href="/blog"
-                class="inline-flex items-center justify-center rounded-lg border border-gray-300 px-5 py-3 text-center text-base font-medium text-gray-900 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+                className="inline-flex items-center justify-center rounded-lg border border-gray-300 px-5 py-3 text-center text-base font-medium text-gray-900 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-800"
               >
                 Go to Blog
                 <svg
-                  class="-mr-1 ml-2 h-5 w-5"
+                  className="-mr-1 ml-2 h-5 w-5"
                   fill="currentColor"
                   viewBox="0 0 20 20"
                   xmlns="http://www.w3.org/2000/svg"
@@ -36,7 +36,7 @@ const Hero = () => {
                 </svg>
               </a>
             </div>
-            <div class="hidden lg:col-span-5 lg:mt-0 lg:flex">
+            <div className="hidden lg:col-span-5 lg:mt-0 lg:flex">
               <dotlottie-player
                 src="https://lottie.host/a4b45020-cc3d-479b-b9c9-bea4ef720150/4bQEIJFbo8.lottie"
                 background="transparent"
