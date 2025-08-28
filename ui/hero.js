e2bffb5f6826c2d156b317809ddf95eae07c733b
Kamal Singh <37046527+ksingh-git@users.noreply.github.com>
2025-08-28 13:24:52 +0530

    Update hero.js

--- ui/hero.js
+++ ui/hero.js
@@ -17,24 +17,6 @@ const Hero = () => {
                 Software Engineer with a passion for building scalable web
                 applications and working across the full stack.
               </p>
-              <a
-                href="/blog"
-                className="inline-flex items-center justify-center rounded-lg border border-gray-300 px-5 py-3 text-center text-base font-medium text-gray-900 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-800"
-              >
-                Go to Blog
-                <svg
-                  className="-mr-1 ml-2 h-5 w-5"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-              </a>
             </div>
             <div className="hidden lg:col-span-5 lg:mt-0 lg:flex">
               <dotlottie-player
