809be232e327caf6ae23bac526192ffce82dbb63
Kamal Singh <37046527+ksingh-git@users.noreply.github.com>
2025-08-28 13:13:14 +0530

    Icons

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Layout from "../components/layout";
 import Hero from "../ui/hero";
+import Footer from "../ui/footer";
+import Projects from "../ui/projects";
 
 export default function Home() {
     return (
         <Layout>
             <Hero/>
+            <Footer/>
         </Layout>
     );
 }
