486faeba9790140fad18fc0d3d6b7bdefacaf6b3
Kamal Singh <37046527+ksingh-git@users.noreply.github.com>
2025-08-30 12:20:23 +0530

    new icons

--- components/itemPost.js
+++ components/itemPost.js
@@ -1,95 +1,104 @@
 import Link from "next/link";
 import Date from "../components/date";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faPython,
-  faMedapps,
-  faJs,
-  faHtml5,
-  faCss3Alt,
-  faReact,
-  faAws,
-  faAngular,
-  faGithub,
-  faGit,
-  faConnectdevelop,
-} from "@fortawesome/free-brands-svg-icons";
+import PythonIcon from "../icons/language/pythonIcon";
+import AwsIcon from "../icons/language/awsIcon";
+import JavascriptIcon from "../icons/language/javascriptIcon";
+import HtmlIcon from "../icons/language/htmlIcon";
+import CssIcon from "../icons/language/cssicon";
+import ReactIcon from "../icons/language/reactIcon";
+import AngularIcon from "../icons/language/angularIcon";
+import GitIcon from "../icons/language/gitIcon";
+import GithubIcon from "../icons/language/githubIcon";
+import AiIcon from "../icons/language/aiIcon";
+import DefaultIcon from "../icons/language/defaultIcon";
+import SecurityIcon from "../icons/language/securityIcon";
+import JavaIcon from "../icons/language/javaIcon";
 
 export default function ItemPost({
-  id,
-  date,
-  title,
-  description,
-  language,
-  categories,
-}) {
-  return (
-    <div
-      key={id}
-      className="p-4 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-105"
-    >
-      <article className="flex h-full flex-col rounded-lg border border-gray-200 bg-white p-6 shadow-md dark:border-gray-700 dark:bg-gray-800">
-        <div className="mb-5 flex items-center justify-between text-gray-500">
-          <span className="bg-primary-100 text-primary-800 dark:bg-primary-200 dark:text-primary-800 inline-flex items-center justify-center rounded px-2.5 py-0.5 text-xl font-medium">
+                                     id,
+                                     date,
+                                     title,
+                                     description,
+                                     language,
+                                     categories,
+                                 }) {
+    return (
+        <div
+            key={id}
+            className="p-4 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-105"
+        >
+            <article
+                className="flex h-full flex-col rounded-lg border border-gray-200 bg-white p-6 shadow-md dark:border-gray-700 dark:bg-gray-800">
+                <div className="mb-5 flex items-center justify-between text-gray-500">
+          <span
+              className="bg-primary-100 text-primary-800 dark:bg-primary-200 dark:text-primary-800 inline-flex items-center justify-center rounded px-2.5 py-0.5 text-xl font-medium">
             <span
-              className="mb-1 mr-1 h-6 w-6"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
+                className="mb-1 mr-1 h-6 w-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
             >
-              {getLanguageIcon({ language })}
+              {getLanguageIcon({language})}
             </span>
           </span>
-          <span className="select-none text-xl">
-            <Date dateString={date} />
+                    <span className="select-none text-xl">
+            <Date dateString={date}/>
           </span>
-        </div>
-        <Link href={`/posts/${id}`}>
-          <h2 className="mb-2 cursor-pointer text-4xl font-bold tracking-tight text-gray-900 hover:underline dark:text-white">
-            {title}
-          </h2>
-        </Link>
-        <p className="mb-5 grow text-xl font-light text-gray-500 dark:text-gray-400">
-          {description}
-        </p>
-        <div className="flex select-none">
-          {categories.map((category) => (
-            <span className="p-2" key={category}>
+                </div>
+                <Link href={`/posts/${id}`}>
+                    <h2 className="mb-2 cursor-pointer text-4xl font-bold tracking-tight text-gray-900 hover:underline dark:text-white">
+                        {title}
+                    </h2>
+                </Link>
+                <p className="mb-5 grow text-xl font-light text-gray-500 dark:text-gray-400">
+                    {description}
+                </p>
+                <div className="flex select-none">
+                    {categories.map((category) => (
+                        <span className="p-2" key={category}>
               <Link key={category} href={`/category/${category}`}>
-                <span className="text-s mr-2 rounded bg-gray-100 px-2.5 py-0.5 font-semibold text-gray-800 dark:bg-gray-700 dark:text-gray-300">
+                <span
+                    className="text-s mr-2 rounded bg-gray-100 px-2.5 py-0.5 font-semibold text-gray-800 dark:bg-gray-700 dark:text-gray-300">
                   #{category}
                 </span>
               </Link>
             </span>
-          ))}
+                    ))}
+                </div>
+            </article>
         </div>
-      </article>
-    </div>
-  );
-  function getLanguageIcon({ language }) {
-    switch (language.toLowerCase()) {
-      case "python":
-        return <FontAwesomeIcon icon={faPython} />;
-      case "aws":
-        return <FontAwesomeIcon icon={faAws} />;
-      case "javascript":
-        return <FontAwesomeIcon icon={faJs} />;
-      case "html":
-        return <FontAwesomeIcon icon={faHtml5} />;
-      case "css":
-        return <FontAwesomeIcon icon={faCss3Alt} />;
-      case "react":
-        return <FontAwesomeIcon icon={faReact} />;
-      case "angular":
-        return <FontAwesomeIcon icon={faAngular} />;
-      case "git":
-        return <FontAwesomeIcon icon={faGit} />;
-      case "github":
-        return <FontAwesomeIcon icon={faGithub} />;
-      case "ai":
-        return <FontAwesomeIcon icon={faConnectdevelop} />;
-      default:
-        return <FontAwesomeIcon icon={faMedapps} />;
+    );
+
+    function getLanguageIcon({language}) {
+        switch (language.toLowerCase()) {
+            case "python":
+                return <PythonIcon/>;
+            case "aws":
+                return <AwsIcon/>;
+            case "javascript":
+                return <JavascriptIcon/>;
+            case "html":
+                return <HtmlIcon/>;
+            case "css":
+                return <CssIcon/>;
+            case "react":
+                return <ReactIcon/>;
+            case "angular":
+                return <AngularIcon/>;
+            case "git":
+                return <GitIcon/>;
+            case "github":
+                return <GithubIcon/>;
+            case "ai":
+                return <AiIcon/>;
+            case "security":
+                return <SecurityIcon/>;
+            case "automation":
+                return <AiIcon/>;
+            case "java":
+                return <JavaIcon/>;
+            default:
+                return <DefaultIcon/>;
+        }
     }
-  }
 }
---
+++ icons/language/securityIcon.jsx
@@ -0,0 +1,13 @@
+const SecurityIcon = () => {
+    return (<svg xmlns="http://www.w3.org/2000/svg" width={32} height={32} viewBox="0 0 16 16">
+        <g fill="none" strokeLinecap="round" strokeLinejoin="round">
+            <path stroke="#cad3f5"
+                  d="M8.128 14.499h-4.81c-.386 0-.818-.432-.818-.818V2.317c0-.385.432-.817.817-.817h9.37c.385 0 .817.432.817.817v6.866"
+                  strokeWidth={1}></path>
+            <path stroke="#a6da95" d="m9.5 12.5l2 2l4-4" strokeWidth={1}></path>
+        </g>
+        <path fill="none" stroke="#cad3f5" strokeLinecap="round" strokeLinejoin="round"
+              d="m7.172 4.238l-2.486 2.89l2.486 2.89m1.656-5.78l2.486 2.89l-2.486 2.89" strokeWidth={1}></path>
+    </svg>)
+}
+export default SecurityIcon;
