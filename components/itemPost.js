087763b235937d615f3c2737be88fad0bd9784f0
Kamal Singh <37046527+ksingh-git@users.noreply.github.com>
2025-08-30 12:39:48 +0530

    Update itemPost.js

--- components/itemPost.js
+++ components/itemPost.js
@@ -15,21 +15,15 @@ import SecurityIcon from "../icons/language/securityIcon";
 import JavaIcon from "../icons/language/javaIcon";
 
 export default function ItemPost({
-                                     id,
-                                     date,
-                                     title,
-                                     description,
-                                     language,
-                                     categories,
+                                     id, date, title, description, language, categories,
                                  }) {
-    return (
-        <div
-            key={id}
-            className="p-4 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-105"
-        >
-            <article
-                className="flex h-full flex-col rounded-lg border border-gray-200 bg-white p-6 shadow-md dark:border-gray-700 dark:bg-gray-800">
-                <div className="mb-5 flex items-center justify-between text-gray-500">
+    return (<div
+        key={id}
+        className="p-4 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-105"
+    >
+        <article
+            className="flex h-full flex-col rounded-lg border border-gray-200 bg-white p-6 shadow-md dark:border-gray-700 dark:bg-gray-800">
+            <div className="mb-5 flex items-center justify-between text-gray-500">
           <span
               className="bg-primary-100 text-primary-800 dark:bg-primary-200 dark:text-primary-800 inline-flex items-center justify-center rounded px-2.5 py-0.5 text-xl font-medium">
             <span
@@ -41,33 +35,30 @@ export default function ItemPost({
               {getLanguageIcon({language})}
             </span>
           </span>
-                    <span className="select-none text-xl">
+                <span className="select-none text-xl">
             <Date dateString={date}/>
           </span>
-                </div>
-                <Link href={`/posts/${id}`}>
-                    <h2 className="mb-2 cursor-pointer text-4xl font-bold tracking-tight text-gray-900 hover:underline dark:text-white">
-                        {title}
-                    </h2>
-                </Link>
-                <p className="mb-5 grow text-xl font-light text-gray-500 dark:text-gray-400">
-                    {description}
-                </p>
-                <div className="flex select-none">
-                    {categories.map((category) => (
-                        <span className="p-2" key={category}>
+            </div>
+            <Link href={`/posts/${id}`}>
+                <h2 className="mb-2 cursor-pointer text-4xl font-bold tracking-tight text-gray-900 hover:underline dark:text-white">
+                    {title}
+                </h2>
+            </Link>
+            <p className="mb-5 grow text-xl font-light text-gray-500 dark:text-gray-400">
+                {description}
+            </p>
+            <div className="flex select-none">
+                {categories.map((category) => (<span className="p-2" key={category}>
               <Link key={category} href={`/category/${category}`}>
                 <span
                     className="text-s mr-2 rounded bg-gray-100 px-2.5 py-0.5 font-semibold text-gray-800 dark:bg-gray-700 dark:text-gray-300">
                   #{category}
                 </span>
               </Link>
-            </span>
-                    ))}
-                </div>
-            </article>
-        </div>
-    );
+            </span>))}
+            </div>
+        </article>
+    </div>);
 
     function getLanguageIcon({language}) {
         switch (language.toLowerCase()) {
