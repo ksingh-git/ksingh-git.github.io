2bb57223594ff527577e67f717861fc499ef66b3
Kamal Singh <37046527+ksingh-git@users.noreply.github.com>
2025-08-30 13:06:26 +0530

    Update [id].js

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,75 +1,72 @@
 import Layout from "../../components/layout";
-import { getAllPostIds, getPostData } from "../../lib/posts";
+import {getAllPostIds, getPostData} from "../../lib/posts";
 import Head from "next/head";
 import Date from "../../components/date";
 import hljs from "highlight.js/lib/common";
 import "highlight.js/styles/github-dark.css";
-import { useEffect } from "react";
+import {useEffect} from "react";
 import Link from "next/link";
 import ThemeModeSvgComponent from "../../components/theme_mode";
 import MarkDownWrapper from "../../components/markdownWrapper";
-import { addButtonToPre } from "../../components/copyFunctions";
+import {addButtonToPre} from "../../components/copyFunctions";
 import Summarize from "../../components/summarize";
 
-export default function Post({ postData }) {
-  useEffect(() => {
-    hljs.configure({
-      languages: ["python"],
-    });
-    hljs.highlightAll();
+export default function Post({postData}) {
+    useEffect(() => {
+        hljs.configure({
+            languages: ["python"],
+        });
+        hljs.highlightAll();
 
-    const highlights = document.querySelectorAll("pre");
-    highlights.forEach((div) => {
-      div.append(addButtonToPre());
-    });
-  }, []);
+        const highlights = document.querySelectorAll("pre");
+        highlights.forEach((div) => {
+            div.append(addButtonToPre());
+        });
+    }, []);
 
-  return (
-    <Layout>
-      <Head>
-        <title>{postData.title}</title>
-      </Head>
-      <section className="min-w-min bg-white dark:bg-gray-900">
-        <div>
-          <div className="flex w-full cursor-pointer justify-between md:p-8">
-            <Link href={`/blog`}>
+    return (<Layout>
+        <Head>
+            <title>{postData.title}</title>
+        </Head>
+        <section className="min-w-min bg-white dark:bg-gray-900">
+            <div>
+                <div className="flex w-full cursor-pointer justify-between md:p-8">
+                    <Link href={`/blog`}>
               <span className="p-2 text-2xl text-black dark:text-white md:pl-8">
                 {"<"} Back to Home
               </span>
-            </Link>
-            <div className="p-2 md:pl-8">
-              <ThemeModeSvgComponent />
+                    </Link>
+                    <div className="p-2 md:pl-8">
+                        <ThemeModeSvgComponent/>
+                    </div>
+                </div>
+                <div className="flex w-full flex-col items-center p-8 md:items-center lg:px-60">
+                    <MarkDownWrapper>
+                        <div className="flex w-full flex-col items-center">
+                            <Date dateString={postData.date}/>
+                            <h1 className="text-center">{postData.title}</h1>
+                        </div>
+                        <Summarize content={postData.contentHtml}/>
+                        <div dangerouslySetInnerHTML={{__html: postData.contentHtml}}/>
+                    </MarkDownWrapper>
+                </div>
             </div>
-          </div>
-          <div className="flex w-full flex-col items-center p-8 md:items-center lg:px-60">
-            <MarkDownWrapper>
-              <div className="flex w-full flex-col items-center">
-                <Date dateString={postData.date} />
-                <h1 className="text-center">{postData.title}</h1>
-              </div>
-              <Summarize content={postData.contentHtml} />
-              <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-            </MarkDownWrapper>
-          </div>
-        </div>
-      </section>
-    </Layout>
-  );
+        </section>
+    </Layout>);
 }
 
-export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id);
-  return {
-    props: {
-      postData,
-    },
-  };
+export async function getStaticProps({params}) {
+    const postData = await getPostData(params.id);
+    return {
+        props: {
+            postData,
+        },
+    };
 }
 
 export async function getStaticPaths() {
-  const paths = getAllPostIds();
-  return {
-    paths,
-    fallback: false,
-  };
+    const paths = getAllPostIds();
+    return {
+        paths, fallback: false,
+    };
 }
