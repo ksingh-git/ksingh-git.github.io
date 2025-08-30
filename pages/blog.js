2fc80061eb2255cff430754ce24bf4bfff1c202b
Kamal Singh <37046527+ksingh-git@users.noreply.github.com>
2025-08-30 13:05:51 +0530

    Update blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -11,8 +11,7 @@ export async function getStaticProps() {
     const allCategories = await getCategoriesList();
     return {
         props: {
-            allPostsData,
-            allCategories,
+            allPostsData, allCategories,
         },
     };
 }
@@ -33,107 +32,81 @@ export default function Home({allPostsData, allCategories}) {
         setSearchTerm("");
     };
 
-    return (
-        <Layout>
-            <Head>
-                <title>Snippets</title>
-            </Head>
-            <div
-                className="relative flex min-h-screen w-full cursor-default content-center justify-center bg-white align-middle dark:bg-gray-900">
-                <div className="relative mx-auto my-8 max-w-screen-xl px-4 py-8 md:m-8 lg:px-6 lg:py-16">
-                    <div className="absolute right-2 top-0">
-                        <ThemeModeSvgComponent className="cursor-pointer"/>
-                    </div>
-                    <h1 className="mx-auto mb-4 max-w-screen-sm select-none text-center text-7xl font-extrabold tracking-tight text-gray-900 dark:text-white">
-                        Snippets
-                    </h1>
-
-                    {/* Search Bar - mobile and tab screens */}
-                    <div className="mx-auto mb-4 max-w-screen-lg tracking-tight text-gray-900 dark:text-white">
-                        <div className="top-2 z-auto content-center backdrop-blur-sm">
-                            <input
-                                value={searchTerm}
-                                onChange={(e) => {
-                                    setSearchTerm(e.target.value);
-                                }}
-                                type="search"
-                                className="text-md block w-full max-w-5xl rounded-lg border border-gray-300 bg-gray-50 p-4 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-                                placeholder="Search"
-                                required=""
-                            />
-                        </div>
-                    </div>
-                    {/* Categories */}
-                    <ul className="my-8 flex select-none flex-wrap justify-center text-center font-medium text-gray-500 dark:text-gray-400">
-                        {allCategories.map((category) => (
-                            <li className="mr-2" key={category}>
-                                <button
-                                    onClick={() => {
-                                        selectedCategory !== category
-                                            ? setSelectedCategory(category)
-                                            : setSelectedCategory("");
-                                    }}
-                                    type="button"
-                                    className={`mb-2 mr-2 rounded-full border border-gray-200 bg-white px-5 py-2.5 text-lg font-medium  hover:bg-gray-100 hover:text-blue-700 ${
-                                        selectedCategory === category
-                                            ? "z-10 text-blue-700 outline-none ring-4 ring-gray-400 dark:text-white dark:ring-gray-700"
-                                            : "text-gray-900 dark:text-gray-400"
-                                    }  dark:border-gray-600 dark:bg-gray-800  dark:hover:bg-gray-700 dark:hover:text-white `}
-                                >
-                                    {category}
-                                </button>
-                            </li>
-                        ))}
-                    </ul>
-                    {/* Posts */}
-                    <div className="min-w-200 grid gap-8 lg:grid-cols-2">
-                        {allPostsData.map(
-                            ({id, date, title, description, language, categories}) => {
-                                if (
-                                    (selectedCategory === "" ||
-                                        categoryExists(selectedCategory, categories)) &&
-                                    (searchTerm === "" ||
-                                        searchTermCheck(searchTerm, [
-                                            title,
-                                            description,
-                                            language,
-                                            categories.join(" "),
-                                        ]))
-                                ) {
-                                    numberOfPosts++;
-                                    return (
-                                        <ItemPost
-                                            key={id}
-                                            id={id}
-                                            date={date}
-                                            title={title}
-                                            description={description}
-                                            language={language}
-                                            categories={categories}
-                                        />
-                                    );
-                                }
-                            }
-                        )}
-                    </div>
-                    {!numberOfPosts ? (
-                        <div className="flex flex-col items-center">
-                            <p className="mb-2 text-center text-3xl font-normal text-gray-500 dark:text-gray-400 sm:text-2xl lg:text-4xl xl:text-4xl">
-                                No Results
-                            </p>
-                            <button
-                                type="button"
-                                onClick={handleClear}
-                                className="rounded-lg border border-gray-300 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-                            >
-                                CLEAR
-                            </button>
-                        </div>
-                    ) : null}
+    return (<Layout>
+        <Head>
+            <title>Snippets</title>
+        </Head>
+        <div
+            className="relative flex min-h-screen w-full cursor-default content-center justify-center bg-white align-middle dark:bg-gray-900">
+            <div className="relative mx-auto my-8 max-w-screen-xl px-4 py-8 md:m-8 lg:px-6 lg:py-16">
+                <div className="absolute right-2 top-0">
+                    <ThemeModeSvgComponent className="cursor-pointer"/>
                 </div>
+                <h1 className="mx-auto mb-4 max-w-screen-sm select-none text-center text-7xl font-extrabold tracking-tight text-gray-900 dark:text-white">
+                    Snippets
+                </h1>
+
+                {/* Search Bar - mobile and tab screens */}
+                <div className="mx-auto mb-4 max-w-screen-lg tracking-tight text-gray-900 dark:text-white">
+                    <div className="top-2 z-auto content-center backdrop-blur-sm">
+                        <input
+                            value={searchTerm}
+                            onChange={(e) => {
+                                setSearchTerm(e.target.value);
+                            }}
+                            type="search"
+                            className="text-md block w-full max-w-5xl rounded-lg border border-gray-300 bg-gray-50 p-4 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                            placeholder="Search"
+                            required=""
+                        />
+                    </div>
+                </div>
+                {/* Categories */}
+                <ul className="my-8 flex select-none flex-wrap justify-center text-center font-medium text-gray-500 dark:text-gray-400">
+                    {allCategories.map((category) => (<li className="mr-2" key={category}>
+                        <button
+                            onClick={() => {
+                                selectedCategory !== category ? setSelectedCategory(category) : setSelectedCategory("");
+                            }}
+                            type="button"
+                            className={`mb-2 mr-2 rounded-full border border-gray-200 bg-white px-5 py-2.5 text-lg font-medium  hover:bg-gray-100 hover:text-blue-700 ${selectedCategory === category ? "z-10 text-blue-700 outline-none ring-4 ring-gray-400 dark:text-white dark:ring-gray-700" : "text-gray-900 dark:text-gray-400"}  dark:border-gray-600 dark:bg-gray-800  dark:hover:bg-gray-700 dark:hover:text-white `}
+                        >
+                            {category}
+                        </button>
+                    </li>))}
+                </ul>
+                {/* Posts */}
+                <div className="min-w-200 grid gap-8 lg:grid-cols-2">
+                    {allPostsData.map(({id, date, title, description, language, categories}) => {
+                        if ((selectedCategory === "" || categoryExists(selectedCategory, categories)) && (searchTerm === "" || searchTermCheck(searchTerm, [title, description, language, categories.join(" "),]))) {
+                            numberOfPosts++;
+                            return (<ItemPost
+                                key={id}
+                                id={id}
+                                date={date}
+                                title={title}
+                                description={description}
+                                language={language}
+                                categories={categories}
+                            />);
+                        }
+                    })}
+                </div>
+                {!numberOfPosts ? (<div className="flex flex-col items-center">
+                    <p className="mb-2 text-center text-3xl font-normal text-gray-500 dark:text-gray-400 sm:text-2xl lg:text-4xl xl:text-4xl">
+                        No Results
+                    </p>
+                    <button
+                        type="button"
+                        onClick={handleClear}
+                        className="rounded-lg border border-gray-300 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+                    >
+                        CLEAR
+                    </button>
+                </div>) : null}
             </div>
-        </Layout>
-    );
+        </div>
+    </Layout>);
 }
 
 function searchTermCheck(searchTerm, includeList) {
@@ -146,7 +119,5 @@ function searchTermCheck(searchTerm, includeList) {
 }
 
 function categoryExists(selectedCategory, categories) {
-    return categories.some(
-        (category) => category.toLowerCase() === selectedCategory.toLowerCase()
-    );
+    return categories.some((category) => category.toLowerCase() === selectedCategory.toLowerCase());
 }
