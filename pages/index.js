eeb4abf0a2662467555d744f8a70f57a7c7a9de6
Kamal Singh <37046527+ksingh-git@users.noreply.github.com>
2025-09-03 20:06:33 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,21 @@ import Layout from "../components/layout";
 import Hero from "../ui/hero";
 import Experience from "../ui/experience";
 import Education from "../ui/education";
+import Skills from "../ui/skills";
 
 export default function Home() {
     return (<Layout>
         {/* Scroll container */}
         <div
-            className=" bg-white dark:bg-gray-900 snap-none lg:h-screen lg:snap-y lg:snap-mandatory lg:overflow-y-scroll">
+            className="bg-white dark:bg-gray-900 snap-none lg:h-screen lg:snap-y lg:snap-mandatory lg:overflow-y-scroll">
             {/* Each section should fill screen height and snap */}
-            <section className="lg:h-screen lg:snap-start">
+            <section className="content-center lg:h-screen lg:snap-start">
                 <Hero/>
             </section>
-            <section className="lg:h-screen lg:snap-start">
+            <section className="content-center lg:h-screen lg:snap-start">
                 <Experience/>
             </section>
-            <section className="lg:h-screen lg:snap-start">
+            <section className="content-center lg:h-screen lg:snap-start">
                 <Education/>
             </section>
         </div>
