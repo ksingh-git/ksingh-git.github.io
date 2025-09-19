5f4bbaeffbabea875d22270f0b0d72214fafbb2b
Kamal Singh <37046527+ksingh-git@users.noreply.github.com>
2025-09-19 20:23:25 +0530

    Add PWA support with offline fallback
    Integrated next-pwa into Next.js configuration to enable Progressive Web App features, including offline support via a new offline.html fallback page. Updated dependencies in package.json and package-lock.json to include next-pwa.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
-module.exports = {
-  assetPrefix: "",
-  images: {
-    unoptimized: true,
-  },
-  devIndicators: {
-    buildActivity: false,
-  },
-};
+const withPWA = require("next-pwa")({
+    dest: "public", register: true, skipWaiting: true, fallbacks: {
+        document: "/offline.html", // when offline
+    },
+});
+
+module.exports = withPWA({
+    reactStrictMode: true, assetPrefix: "", images: {
+        unoptimized: true,
+    }, devIndicators: {
+        buildActivity: false,
+    },
+});
+
