f7119fa3926d966bea05dac92f7d6c9a52ae4dba
Kamal Singh <37046527+ksingh-git@users.noreply.github.com>
2025-09-19 20:40:25 +0530

    Revert "Add PWA support with offline fallback"
    This reverts commit 5f4bbaeffbabea875d22270f0b0d72214fafbb2b.

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
-const withPWA = require("next-pwa")({
-    dest: "public", register: true, skipWaiting: true, fallbacks: {
-        document: "/offline.html", // when offline
-    },
-});
-
-module.exports = withPWA({
-    reactStrictMode: true, assetPrefix: "", images: {
-        unoptimized: true,
-    }, devIndicators: {
-        buildActivity: false,
-    },
-});
-
+module.exports = {
+  assetPrefix: "",
+  images: {
+    unoptimized: true,
+  },
+  devIndicators: {
+    buildActivity: false,
+  },
+};
