0711d20c0accf61b033464e32129a80057bfb933
Kamal Singh <37046527+ksingh-git@users.noreply.github.com>
2025-09-20 22:34:34 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Experience from "../ui/experience";
 import Education from "../ui/education";
 import Projects from "../ui/projects";
 import {useEffect, useState} from "react";
+import Head from "next/head";
 
 export default function Home() {
     const [loading, setLoading] = useState(true);
@@ -16,6 +17,9 @@ export default function Home() {
         return () => clearTimeout(timer);
     }, []);
     return (<Layout>
+        <Head>
+            <title>Kamal Singh</title>
+        </Head>
         {loading ? <div className="bg-white dark:bg-gray-900 h-screen w-screen flex items-center justify-center">
             <dotlottie-player
                 src="https://lottie.host/e752b4e6-1123-4ef3-81dd-3d9b206d45b2/TqZvAl1zPY.lottie"
